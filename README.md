df8760088189004faa46a5df12549f0320ee0fc1
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-04-05 19:17:09 +0530

    Feat: Create and Read functionality is done!

--- README.md
+++
@@ -1,2 +0,0 @@
-# Aman-Javascript
-This is my JavaScript Repo and I will be pushing here my daily tasks and practicals.
--- index.js
+++ index.js
@@ -1,71 +1,86 @@
+let d = new Date()
+let uniqueID = d.getTime()
 
-const uniqueID = () => {
-    const date = new Date();
-    const uniqueKey = date.getTime()
-    return uniqueKey
-}
+let localStorage_array = JSON.parse(localStorage.getItem("crud")) || []
 
 const submit = () => {
-    let dataEntered = retrieveData();
-    let readData = readingDataFromLS(dataEntered)
-    console.log(readData,"ppppp")
-    insertData(readData)
+
+    let dataEntered = retrieveData();         //data_object is getting stored
+
+    localStorage_array.push(dataEntered)                // pushing all objects in an array
+
+    setDataToLocalStorage(localStorage_array)                    // We are setting the array data in the localStorage
+
+    let localStorage_getData = getDataFromLocalStorage(localStorage_array)     // We are getting the data from the localStorage
+
+    insertProductData(localStorage_getData)                // Calling the insert Product function to add the products in the DOM
+
 }
 
 //Creating:
 
-const retrieveData = () =>{
-let pid = document.getElementById("pid").value;
-let pname = document.getElementById("pname").value;
-let pprice = document.getElementById("pprice").value;
-let pimage = document.getElementById("pimage").value;
-let pdesc = document.getElementById("pdesc").value;
+const retrieveData = () => {
+    let pid = document.getElementById("pid").value;
+    let pname = document.getElementById("pname").value;
+    let pprice = document.getElementById("pprice").value;
+    let pimage = document.getElementById("pimage").value;
+    let pdesc = document.getElementById("pdesc").value;
 
-let data_array = [pid,pname,pprice,pimage,pdesc]
-return data_array
+    // storing all the values 
+    let data_object = {
+        pid: pid,
+        pname: pname,
+        pprice: pprice,
+        pimage: pimage,
+        pdesc: pdesc
+    }
+    return data_object
 }
-
 
 //Data in the LocalStorage (LS)
 
-const readingDataFromLS = (dataEntered) => {
+const setDataToLocalStorage = (localStorage_array) => {
 
     //storing data in the LS
-    let pidLS = localStorage.setItem("pid",dataEntered[0])
-    let pnameLS = localStorage.setItem("pname",dataEntered[1])
-    let ppriceLS = localStorage.setItem("pprice",dataEntered[2])
-    let pimageLS = localStorage.setItem("pimage",dataEntered[3])
-    let pdescLS = localStorage.setItem("pdesc",dataEntered[4])
+    localStorage.setItem("crud", JSON.stringify(localStorage_array))
+}
+
+const getDataFromLocalStorage = () => {
 
     //getting data from the LS
-    let pid_GLS = localStorage.getItem("pid",pidLS)
-    let pname_GLS = localStorage.getItem("pname",pnameLS)
-    let pprice_GLS = localStorage.getItem("pprice",ppriceLS)
-    let pimage_GLS = localStorage.getItem("pimage",pimageLS)
-    let pdesc_GLS = localStorage.getItem("pdesc",pdescLS)
-
-    let getdata_array = [pid_GLS,pname_GLS,pprice_GLS,pimage_GLS,pdesc_GLS]
-    return getdata_array
+    let ls_getData = JSON.parse(localStorage.getItem("crud"))
+    console.log(ls_getData, "ls_getData")
+    return ls_getData
 }
 
 
-const insertData = (readData) =>{
-    let row = table.insertRow();
-    console.log(readData[0],readData[1],readData[2],readData[4])
-    row.insertCell(0).innerHTML = readData[0];
-    row.insertCell(1).innerHTML = readData[1];
-    row.insertCell(2).innerHTML = readData[2];
-    row.insertCell(3).innerHTML = readData[3];
-    row.insertCell(4).innerHTML = readData[4];
-    row.insertCell(5).innerHTML = `<button onclick=edit(this)>Edit</button><button onclick=delete(this)>Delete</button>`;
+const insertProductData = (localStorage_getData) => {
 
-};
+    let product_items = document.getElementById("product-items")
+    product_items.innerHTML = ""
 
-const edit =(td) =>{
-    let row = td.parentElement.parentElement;
-    document.getElementById("pid").value = row.cells[0].innerHTML;
-    document.getElementById("pname").value = row.cells[1].innerHTML;
-    document.getElementById("pprice").value = row.cells[2].innerHTML;
-    document.getElementById("pimage").value = row.cells[3].innerHTML;
-    document.getElementById("pdesc").value = row.cells[4].innerHTML;
+    localStorage_getData.forEach(element =>  {
+
+        if(element.pprice == ""){
+            element.pprice = 0.01;
+        }
+
+        let dynamicProduct = document.createElement("div");
+
+        dynamicProduct.innerHTML += `<div class="col">
+        <div class="card">
+        <img class="card-img-top" src="${element.pimage}" alt="Card image cap">
+        <div class="card-body">
+        <h6 class="card-title" id="pid" alt="hhhhhhhhh">${element.pid}</h6>
+        <h4 class="card-title" id="pname">${element.pname}</h4>
+        <p class="card-text" id="pdesc">${element.pdesc}</p>
+        <a href="#" class="btn btn-primary" id="pprice">${element.pprice}</a>
+        </div>
+        </div>
+        </div>`
+        
+        product_items.append(dynamicProduct);
+    })
 }
+
+insertProductData(localStorage_array)
