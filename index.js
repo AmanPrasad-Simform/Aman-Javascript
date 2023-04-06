bf6ff670f2289bdf21feba5b9058222e6ec4f410
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-04-06 11:48:38 +0530

    perf: improved the code quality, worked on naming conventions

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const links = document.querySelectorAll(".nav-link")
+const bars = document.querySelectorAll(".myBar");
+const mybutton = document.getElementsByClassName("scrolltopbtn")
+const navbarht = document.querySelector(".navbar")
+const ht = navbarht.offsetHeight;
+const tabcontent = document.querySelectorAll(".tab-contents");
+const toggleTabs = document.querySelector(".tab-titles");
+
 const slides = document.getElementsByClassName("slider-item");
 const nextButton = document.getElementById("slider-button-next");
 const prevButton = document.getElementById("slider-button-prev");
 const dots = document.getElementsByClassName("dot");
-let position = 0;
+let position = 0;   // we are changing it that's why "let"
 const numberOfSlides = slides.length;
 
 function hideAllSlides() {
@@ -12,10 +20,8 @@ function hideAllSlides() {
         slide.classList.add("slider-item-hidden");
     }
 }
-
 const handleMoveToNextSlide = function (e) {
     hideAllSlides();
-
     // check if last slide has been reached
     if (position === numberOfSlides - 1) {
         position = 0; // go back to first slide
@@ -25,15 +31,12 @@ const handleMoveToNextSlide = function (e) {
     }
     // make current slide visible
     slides[position].classList.add("slider-item-visible");
-
     // update dot to represent current slide
     dots[position].classList.add("selected-dot");
     dots[position].checked = true;
 }
-
 const handleMoveToPrevSlide = function (e) {
     hideAllSlides();
-
     // check if we're on the first slide
     if (position === 0) {
         position = numberOfSlides - 1; // move to the last slide
@@ -43,21 +46,21 @@ const handleMoveToPrevSlide = function (e) {
     }
     // make current slide visible
     slides[position].classList.add("slider-item-visible");
-
     // update dot to represent current slide
     dots[position].classList.add("selected-dot");
     dots[position].checked = true;
 }
-
 // listen for slide change events
 nextButton.addEventListener("click", handleMoveToNextSlide);
 prevButton.addEventListener("click", handleMoveToPrevSlide);
 
-var id = "";
-function myMove() {
-    var flag = false
-    var elem = document.getElementById("myAnimation");
-    var pos = 0;
+
+//The tech animation which we see on the homepage
+let id = "";
+function techAnimation() {
+    let flag = false
+    let elem = document.getElementById("myAnimation");
+    let pos = 0;
     clearInterval(id);
     id = setInterval(frame, 2);
     function frame() {
@@ -72,42 +75,54 @@ function myMove() {
             elem.style.left = pos + 'px';
         }
     }
-    function flagtrue() {
-        var flagbtn = document.getElementById("flagbtn")
-        flagbtn.style.visibility = "visible";
-        redboxanimation()
+}
+function flagtrue() {
+    let flagbtn = document.getElementById("flagbtn")
+    flagbtn.style.visibility = "visible";
+    boxAnimation()
+}
+function boxAnimation() {
+    let redbox = document.getElementById("myAnimation")
+    setTimeout(() => {
+        redbox.style.visibility = "hidden";
+    }, 100)
+    redbox.style.transition = "ease"
+}
+
+// The rating bars which is on the homepage
+function animateBars() {
+    for (barItem of bars) {
+        width = barItem.getAttribute("data-status");
+        console.log('in for each loop ==== ', barItem, '=== width ==== ', width);
+        barItem.style.width = width + "%";
+        barItem.innerHTML = width + "%";
+        barItem.style.transition = 'all 1s ease-in'
     }
 }
 
-let elem = document.querySelectorAll(".myBar");
-let i = 0;
-function move() {
-    if (i == 0) {
-        i = 1;
-        for (value of elem) {
-            var widd = 10;
-            var id1 = setInterval(progress, 10);
-            function progress() {
-                if (widd >= 100) {
-                    clearInterval(id1);
-                    i = 0;
-                } else {
-                    elem.forEach(value => {
-                        const widd = value.getAttribute("data-status");
-                        value.style.width = widd + "%";
-                        value.innerHTML = widd + "%";
-                        value.style.transition = 'all 1s ease-in'
-                    });
-                }
-            }
-
-        }
-    }
+// Function to reset the homepage animation
+function resetAnimationBars() {
+    document.querySelectorAll(".myBar").forEach((element) => {
+        console.log('Element Style', element);
+        element.style = { width: '10%' }
+        element.innerHTML = '10%';
+    });
 }
 
+// Function to call the animations again if the id==home is clicked
+function animationCall(id) {
+    if (id == 'home') {
+        resetAnimationBars();
+        setTimeout(() => {
+            techAnimation();
+            animateBars();
+        }, 100);
+    }
+    window.scroll(0, Position(document.getElementById(id)));
+}
 
-let mybutton = document.getElementsByClassName("scrolltopbtn")
-window.onscroll = function () { scrollFunction() };
+//function to show or hide the scrolltop button and executing it
+window.onscroll = function () { scrollFunction(); };
 function scrollFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         mybutton[0].style.visibility = "visible";
@@ -117,24 +132,9 @@ function scrollToTop() {
     window.scrollTo(0, 0);
 }
 
-
-function redboxanimation() {
-    var redbox = document.getElementById("myAnimation")
-    setTimeout(() => {
-        redbox.style.visibility = "hidden";
-    }, 100)
-    redbox.style.transition = "ease"
-}
-
-function scrollElement(id) {
-    window.scroll(0, Position(document.getElementById(id)));
-}
-
-const navbarht = document.querySelector(".navbar")
-
-var ht = navbarht.offsetHeight;
+//function to scroll the element to it's height using it's ID
 function Position(obj) {
-    var currenttop = 0;
+    let currenttop = 0;
     if (obj.offsetParent) {
         do {
             currenttop += obj.offsetTop;
@@ -143,42 +143,32 @@ function Position(obj) {
     }
 }
 
-const links = document.querySelectorAll(".nav-link")
 for (const value of links) {
-    console.log(value.getAttribute("data-custom"), "GAGGGAGAAG")
     value.addEventListener("click", () => {
-        console.log(value.getAttribute("data-custom"))
         scrollElement(value.getAttribute("data-custom"))
+        animationCall(value.getAttribute("data-custom"))
     })
 }
 
-//    document.querySelectorAll('.nav-link[data-custom]').forEach(element => {
-//     element.addEventListener('click', function (event) {
-//         document.getElementById(this.getAttribute('data-custom')).scrollIntoView({ behavior: "smooth", inline: "nearest" });
-//     });
-// });
+function scrollElement(id) {
+    window.scroll(0, Position(document.getElementById(id)));
+}
 
-const tabcontent = document.querySelectorAll(".tab-contents");
-const toggleTabs = document.querySelector(".tab-titles");
+// Toggle the about section 
 toggleTabs.addEventListener("click", (e) => {
-
     const removeClass = toggleTabs.children;
-
-  if (e.target.children.length === 0) {
-
-    for (let element of removeClass) {
-      element.classList.remove("active-link");
+    if (e.target.children.length === 0) {
+        for (let element of removeClass) {
+            element.classList.remove("active-link");
+        }
+        tabcontent.forEach((element) => {
+            element.classList.remove("active-tab");
+        });
+        document
+            .getElementById(`${e.target.innerText.toLowerCase()}`)
+            .classList.add("active-tab");
+        e.target.classList.add("active-link");
+    } else {
+        return;
     }
-
-    tabcontent.forEach((element) => {
-      element.classList.remove("active-tab");
-    });
-    document
-      .getElementById(`${e.target.innerText.toLowerCase()}`)
-      .classList.add("active-tab");
-      
-    e.target.classList.add("active-link");
-  } else {
-    return;
-  }
 });
