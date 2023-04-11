c9ac0c037b58504980528cb7aaae48b1cb044b85
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-04-11 19:11:02 +0530

    Style: Improved UI and removed product show from the main page

--- assets/js/view.js
+++ assets/js/view.js
@@ -1,5 +1,7 @@
 let localStorage_array = JSON.parse(localStorage.getItem("crud")) || [];
-
+// if(localStorage_array.length == 0){
+//      localStorage_array = "nothing"
+// }
 //Data in the LocalStorage (LS)
 
 const setDataToLocalStorage = (localStorage_array) => {
@@ -10,40 +12,51 @@ const setDataToLocalStorage = (localStorage_array) => {
 
 const getDataFromLocalStorage = () => {
   //getting data from the LS
-  const localStorage_getData = JSON.parse(localStorage.getItem("crud"));
+  const localStorage_getData = JSON.parse(localStorage.getItem("crud")) || [];
   return localStorage_getData;
 };
 
 // To show all the products in view page
 const insertProductData = (localStorage_getData) => {
-  let product_items = document.getElementById("product-items");
-  product_items.innerHTML = "";
 
-  localStorage_getData.forEach((element) => {
-    let dynamicProduct = document.createElement("div");
-    
-    if(element.pdesc == ""){
-        element.pdesc = "<b>There is no description.</b>"
+    let product_items = document.getElementById("product-items");
+    product_items.innerHTML = "";
+
+    if(localStorage_getData.length == 0){
+console.log("here") 
+       product_items.innerHTML = `<img class="nothingToSeeImg mx-auto" src="../nothingtosee.gif"  alt="No image">` 
+    }else{
+console.log(localStorage_array)
+        
+        
+        localStorage_getData.forEach((element) => {
+            let dynamicProduct = document.createElement("div");
+            
+            if(element.pdesc == ""){
+                element.pdesc = "<b>There is no description.</b>"
+            }
+            
+            dynamicProduct.innerHTML += `<div class="col" id="productData">
+            <div class="card text-center">
+            <div class="card-body" style="background-color: ;">
+            <img class="card-img-top" src="${element.pimage}" style="width:130px; height:150px; margin-bottom:10px; object-fit:contain" alt="No image">
+            <h4 class="card-title" id="pid">ID : ${element.pid}</h4>
+            <h6 class="card-title" id="pname">Name : ${element.pname}</h6>
+            <h6 class="card-text" id="pprice">Price : ${element.pprice}</h6>
+            <h6 class="card-text" id="pdesc">Description :<br> ${element.pdesc}</h6>
+            </div>
+            </div>
+            </div>`;
+            
+            product_items.append(dynamicProduct);
+        });
     }
-
-    dynamicProduct.innerHTML += `<div class="col" id="productData">
-        <div class="card text-center">
-        <div class="card-body">
-        <img class="card-img-top" src="${element.pimage}" style="width: 150px; height: 150px;" alt="No image">
-        <h4 class="card-title" id="pid" alt="hhhhhhhhh">ID : ${element.pid}</h4>
-        <h6 class="card-title" id="pname">Name : ${element.pname}</h6>
-        <h6 class="card-text" id="pprice">Price : ${element.pprice}</h6>
-        <p class="card-text" id="pdesc">Description : ${element.pdesc}</p>
-        </div>
-        </div>
-        </div>`;
-
-    product_items.append(dynamicProduct);
-  });
   localStorage.setItem("crud", JSON.stringify(localStorage_array));
 };
+
 // To show the product details for the first time
 insertProductData(localStorage_array);
+
 
 // Filter function on every search input
 const myInput = document.getElementById("myInput");
@@ -76,22 +89,22 @@ const sort = () => {
 
 // Sort Function 
 const sortByAttr = (sortByAttribute) => {
-  localStorage_array.sort((a, b) => {
+  localStorage_array.sort((product1, product2) => {
     if (sortByAttribute == "pname") {
-      let fa = a.pname.toLowerCase();
-      let fb = b.pname.toLowerCase();
-      if (fa > fb) {
+      let productA = product1.pname.toLowerCase();
+      let productB = product2.pname.toLowerCase();
+      if (productA > productB) {
         return 1;
       }
-      if (fa < fb) {
+      if (productA < productB) {
         return -1;
       }
       return 0;
     } else if (sortByAttribute == "pprice") {
-      return a.pprice - b.pprice;
+      return product1.pprice - product2.pprice;
       // return a[`${sortByAttribute}`] - b[`${sortByAttribute}`]
     } else {
-      return a.pid - b.pid;
+      return product1.pid - product2.pid;
     }
   });
   insertProductData(localStorage_array);
