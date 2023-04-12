f782927029bbb58e34cca682ae4d23a2417c613a
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-04-12 18:56:28 +0530

    perf: Improved performance and UI!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
+
+// Function to craete unique ID :
 const uniqueID = () => {
     return Date.now().toString().slice(5);
 }
+
+// Array to store every item of the localStorage :
 let localStorage_array = JSON.parse(localStorage.getItem("crud")) || [];
+
 // Declaring the input field variables MAIN form
 let pid = document.querySelector("#input-fields #pid");
 let pname = document.getElementById("pname");
@@ -11,47 +16,21 @@ let uploadPreview = document.getElementById("uploadPreview");
 let pdesc = document.getElementById("pdesc");
 let formField = document.getElementById("form-field")
 
-// Declaring the input field variables EDIT form 
-let modal = document.getElementById("modalUpdate");
-let pid_update = document.querySelector(`#modalUpdate #pid`);
-let pname_update = document.querySelector(`#modalUpdate #pname`);
-let pprice_update = document.querySelector(`#modalUpdate #pprice`);
-let uploadPreview_update = document.querySelector("#modalUpdate #uploadPreview-update");
-let pimage_update = document.querySelector(`#modalUpdate #pimage_update`);
-let pdesc_update = document.querySelector(`#modalUpdate #pdesc`);
-
 // Execution on submit click in form 
-const submit = () => {
-
-    const dataEntered = retrieveData(); //data_object is getting stored
-
-    localStorage_array.push(dataEntered); // pushing all objects in an array
-
-    setDataToLocalStorage(localStorage_array); // We are setting the array data in the localStorage
-
-    // window.location.href = window.location.host + '/pages/view.html'
-
-    // let localStorage_getData = getDataFromLocalStorage(); // We are getting the data from the localStorage
-
-    // insertProductData(localStorage_getData); // Calling the insert Product function to add the products in the DOM
-
+const submit = async (e) => {
+    e.preventDefault();
+    const dataEntered = retrieveData();  // data_object is getting stored
+    localStorage_array.push(dataEntered);    // pushing all objects in an array   
+    setDataToLocalStorage(localStorage_array);   // We are setting the array data in the localStorage
+    await swal("", "Product added!", "success");    // animation to show on adding product 
+    window.location.href = '/pages/view.html'    // Routing to edit page directly after adding product
 };
 
-// When form is submitted then call submit function
+// When FORM is submitted then call submit function
 formField.addEventListener("submit", submit)
-
-// function to reset the input fields
-const reset = () => {
-    pname.value = "";
-    pprice.value = "";
-    pimage.value = "";
-    uploadPreview.src = "../no_img.jpg";
-    pdesc.value = "";
-};
 
 //Creating :
 const retrieveData = () => {
-
     // storing all the values
     let data_object = {
         pid: uniqueID(),
@@ -70,133 +49,11 @@ const setDataToLocalStorage = (localStorage_array) => {
     localStorage.setItem("crud", JSON.stringify(localStorage_array));
 };
 
-const getDataFromLocalStorage = () => {
-    //getting data from the LS
-    const localStorage_getData = JSON.parse(localStorage.getItem("crud"));
-    return localStorage_getData;
-};
-
 // Function to preview the image in the form filling section
 function PreviewImage() {
     let imgReader = new FileReader();
     imgReader.readAsDataURL(document.getElementById("pimage").files[0]);
     imgReader.onload = function (imgResult) {
         uploadPreview.src = imgResult.target.result;
-        return uploadPreview.src;
     };
 };
-
-// Function to preview the image in the form EDIT section
-function UpdatePreviewImage() {
-    let imgReader = new FileReader();
-    imgReader.readAsDataURL(document.getElementById("pimage_update").files[0]);
-    imgReader.onload = function (imgResult) {
-        uploadPreview_update.src = imgResult.target.result;
-        return uploadPreview_update.src;
-    };
-};
-
-// function to validate the field inputs 
-const validateProduct = (dataEntered) => {
-    console.log(dataEntered, "vall")
-    if (dataEntered.pname == "") {
-        alert("Please enter product name!")
-        return false;
-    }
-    else if (dataEntered.pdesc == "") {
-        dataEntered.pdesc = "<b>There is no description.</b>"
-        return true;
-    }
-    else {
-        return true
-    }
-}
-
-// This function will insert Products in the view area
-const insertProductData = (localStorage_getData) => {
-    let product_items = document.getElementById("product-items");
-    product_items.innerHTML = "";
-
-    localStorage_getData.forEach((element) => {
-        let dynamicProduct = document.createElement("div");
-
-        dynamicProduct.innerHTML += `<div class="col card-column" id="productData">
-        <div class="card text-center">
-        <div class="card-body ">
-        <img class="card-img-top" src="${element.pimage}" style="width: 150px; height: 150px;;" alt="Product Image">
-        <h4 class="card-title" id="pid" alt="hhhhhhhhh">ID : ${element.pid}</h4>
-        <h6 class="card-title" id="pname">Name : ${element.pname}</h6>
-        <h6 class="card-text" id="pprice">Price : ₹ ${element.pprice}</h6>
-        <h6 class="card-text text-truncate"  id="pdesc">Description : <br>${element.pdesc}</h6>
-        <button type="button" id="demo" class="btn btn-success" data-custom-id=${element.pid} onclick="updateProduct('${element.pid}')" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
-        <button type="button" class="btn btn-danger" data-custom-id=${element.pid} onclick="deleteProduct('${element.pid}')">Delete</button>
-        </div>
-        </div>
-        </div>`;
-
-        product_items.append(dynamicProduct);
-    });
-    localStorage.setItem("crud", JSON.stringify(localStorage_array));
-};
-// To show the product details for the first time
-
-
-// To delete the product
-const deleteProduct = (id) => {
-    if (confirm('Are you sure you want to delete the product??')) {
-        localStorage_array = localStorage_array.filter((element) => {
-            return element.pid != id;
-        });
-        localStorage.setItem("crud", JSON.stringify(localStorage_array));
-        insertProductData(localStorage_array);
-    }
-    else {
-        return 0;
-    }
-};
-
-// To update the product 
-const updateProduct = (id) => {
-    const productToBeUpdate = localStorage_array.find((product) => {
-        if (product.pid == id) {
-            return product;
-        }
-    });
-    const index = localStorage_array.map((product) => product.pid).indexOf(id);
-    sessionStorage.setItem("index", JSON.stringify(index));
-    editValue(productToBeUpdate);
-};
-
-// To render the value in the edit modal section
-const editValue = (productToBeUpdate) => {
-    (pid_update.value = productToBeUpdate.pid),
-        (pname_update.value = productToBeUpdate.pname),
-        (pprice_update.value = productToBeUpdate.pprice),
-        (pdesc_update.value = productToBeUpdate.pdesc);
-    (uploadPreview_update.src = productToBeUpdate.pimage);
-    (pimage_update.value = productToBeUpdate.pimage);
-};
-
-// To change the data which is enetered in the localStorage 
-const edit = () => {
-    const index = JSON.parse(sessionStorage.getItem("index"));
-    const tempData = {
-        pid: pid_update.value,
-        pname: pname_update.value,
-        pprice: pprice_update.value,
-        pimage: uploadPreview_update.src,
-        pdesc: pdesc_update.value,
-    };
-    localStorage_array.splice(index, 1, tempData);
-    setDataToLocalStorage(localStorage_array);
-    const localStorage_getData = getDataFromLocalStorage(localStorage_array);
-    insertProductData(localStorage_getData);
-};
-
-// const sortById = () => {
-//     localStorage_array.sort((a, b) => {
-//         return a.pid - b.pid;
-//     })
-//     insertProductData(localStorage_array);
-// }
-// // sortById();
--- assets/js/view.js
+++ assets/js/view.js
@@ -1,19 +1,38 @@
+
+// Array to store every item of the localStorage :
 let localStorage_array = JSON.parse(localStorage.getItem("crud")) || [];
-// if(localStorage_array.length == 0){
-//      localStorage_array = "nothing"
-// }
+
+// Declaring the input field variables EDIT form 
+let myInput = document.getElementById("myInput");
+let modal = document.getElementById("modalUpdate");
+let pid_update = document.querySelector(`#modalUpdate #pid`);
+let pname_update = document.querySelector(`#modalUpdate #pname`);
+let pprice_update = document.querySelector(`#modalUpdate #pprice`);
+let uploadPreview_update = document.querySelector("#modalUpdate #uploadPreview-update");
+let pimage_update = document.querySelector(`#modalUpdate #pimage_update`);
+let pdesc_update = document.querySelector(`#modalUpdate #pdesc`);
+
 //Data in the LocalStorage (LS)
 
 const setDataToLocalStorage = (localStorage_array) => {
-  console.log(localStorage_array, "view");
-  //storing data in the LS
-  localStorage.setItem("crud", JSON.stringify(localStorage_array));
+    //storing data in the LS
+    localStorage.setItem("crud", JSON.stringify(localStorage_array));
 };
 
 const getDataFromLocalStorage = () => {
-  //getting data from the LS
-  const localStorage_getData = JSON.parse(localStorage.getItem("crud")) || [];
-  return localStorage_getData;
+    //getting data from the LS
+    const localStorage_getData = JSON.parse(localStorage.getItem("crud")) || [];
+    return localStorage_getData;
+};
+
+// Function to preview the image in the form EDIT section
+function UpdatePreviewImage() {
+    let imgReader = new FileReader();
+    imgReader.readAsDataURL(document.getElementById("pimage_update").files[0]);
+    imgReader.onload = function (imgResult) {
+        uploadPreview_update.src = imgResult.target.result;
+        return uploadPreview_update.src;
+    };
 };
 
 // To show all the products in view page
@@ -22,90 +41,143 @@ const insertProductData = (localStorage_getData) => {
     let product_items = document.getElementById("product-items");
     product_items.innerHTML = "";
 
-    if(localStorage_getData.length == 0){
-console.log("here") 
-       product_items.innerHTML = `<img class="nothingToSeeImg mx-auto" src="../nothingtosee.gif"  alt="No image">` 
-    }else{
-console.log(localStorage_array)
-        
-        
+    if (localStorage_getData.length == 0) {
+        product_items.innerHTML = `<img class="nothingToSeeImg mx-auto" src="../images/no-products.avif" alt="No image">`
+        document.getElementById("sorting-container").classList.add("d-none")
+    }
+    else {
+        document.getElementById("sorting-container").classList.remove("d-none")
         localStorage_getData.forEach((element) => {
             let dynamicProduct = document.createElement("div");
-            
-            if(element.pdesc == ""){
-                element.pdesc = "<b>There is no description.</b>"
+
+            if (element.pdesc == "") {
+                element.pdesc = "There is no description."
             }
-            
             dynamicProduct.innerHTML += `<div class="col" id="productData">
             <div class="card text-center">
-            <div class="card-body" style="background-color: ;">
-            <img class="card-img-top" src="${element.pimage}" style="width:130px; height:150px; margin-bottom:10px; object-fit:contain" alt="No image">
-            <h4 class="card-title" id="pid">ID : ${element.pid}</h4>
-            <h6 class="card-title" id="pname">Name : ${element.pname}</h6>
-            <h6 class="card-text" id="pprice">Price : ${element.pprice}</h6>
-            <h6 class="card-text" id="pdesc">Description :<br> ${element.pdesc}</h6>
+            <div class="card-body">
+            <div class="card-icons d-flex justify-content-end">
+            <i class="fa-solid fa-trash mx-2" style="color:#d9534f" data-custom-id=${element.pid} onclick="deleteProduct('${element.pid}')"></i>
+            <i class="fa-regular fa-pen-to-square" style="color:#5cb85c" data-custom-id=${element.pid} onclick="updateProduct('${element.pid}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+            </div>
+            <img class="card-img-top" src="${element.pimage}" style="width:130px; height:150px; object-fit:contain;" alt="No image">
+            <h4 class="card-title" id="pid"><b>ID :</b> ${element.pid}</h4>
+            <h6 class="card-title" id="pname"><b>Name :</b> ${element.pname}</h6> 
+            <h6 class="card-text" id="pprice"><b>Price :</b> ${element.pprice}</h6>
+            <h6 class="card-text" id="pdesc"><b>Description :</b><br> ${element.pdesc}</h6>
             </div>
             </div>
             </div>`;
-            
             product_items.append(dynamicProduct);
         });
     }
-  localStorage.setItem("crud", JSON.stringify(localStorage_array));
+    localStorage.setItem("crud", JSON.stringify(localStorage_array));
 };
+
 
 // To show the product details for the first time
 insertProductData(localStorage_array);
 
 
-// Filter function on every search input
-const myInput = document.getElementById("myInput");
+// Function to GET attribute for Filter (Search) function
 myInput.addEventListener("input", (e) => {
-  const select_container = document.getElementById("select-container");
-  const filterAttribute =
-    select_container.options[select_container.selectedIndex].getAttribute(
-      "data-custom"
-    );
-  const temp = searchByAttribute(e.target.value, filterAttribute);
-  insertProductData(temp);
+    const select_container = document.getElementById("select-container");
+    const filterAttribute =
+        select_container.options[select_container.selectedIndex].getAttribute(
+            "data-custom"
+        );
+    const temp = searchByAttribute(e.target.value, filterAttribute);
+    insertProductData(temp);
 });
 
-// Search result on specified attribute
+// Search function on every search input
 const searchByAttribute = (searchQuery, filterAttribute) => {
-  return localStorage_array.filter((product) => {
-    return product[`${filterAttribute}`].toLowerCase().includes(searchQuery);
-  });
+    return localStorage_array.filter((product) => {
+        return product[`${filterAttribute}`].toLowerCase().includes(searchQuery);
+    });
 };
 
-// Function to get attribute for Sort function
+// Function to GET attribute for Sort function
 const sort = () => {
-  const sort_container = document.getElementById("sort-container");
-  const sortAttribute =
-    sort_container.options[sort_container.selectedIndex].getAttribute(
-      "data-custom"
-    );
-  sortByAttr(sortAttribute);
+    const sort_container = document.getElementById("sort-container");
+    const sortAttribute =
+        sort_container.options[sort_container.selectedIndex].getAttribute(
+            "data-custom"
+        );
+    sortByAttr(sortAttribute);
 };
 
 // Sort Function 
 const sortByAttr = (sortByAttribute) => {
-  localStorage_array.sort((product1, product2) => {
-    if (sortByAttribute == "pname") {
-      let productA = product1.pname.toLowerCase();
-      let productB = product2.pname.toLowerCase();
-      if (productA > productB) {
-        return 1;
-      }
-      if (productA < productB) {
-        return -1;
-      }
-      return 0;
-    } else if (sortByAttribute == "pprice") {
-      return product1.pprice - product2.pprice;
-      // return a[`${sortByAttribute}`] - b[`${sortByAttribute}`]
-    } else {
-      return product1.pid - product2.pid;
+
+    localStorage_array.sort((product1, product2) => {
+        if (sortByAttribute == "pname") {
+            let productA = product1[sortByAttribute].toLowerCase();
+            let productB = product2[sortByAttribute].toLowerCase();
+            if (productA > productB) {
+                return 1;
+            }
+            if (productA < productB) {
+                return -1;
+            }
+            return 0;
+        }else {
+            return product1[sortByAttribute] - product2[sortByAttribute];
+        }
+    });
+    insertProductData(localStorage_array);
+};
+
+// To delete the product
+const deleteProduct = async (id) => {
+    if (await swal("", "Are you sure you want to delete this product!")) {
+        localStorage_array = localStorage_array.filter((element) => {
+            return element.pid != id;
+        });
+        localStorage.setItem("crud", JSON.stringify(localStorage_array));
+        insertProductData(localStorage_array);
+        swal("", "Product deleted!", "success");
     }
-  });
-  insertProductData(localStorage_array);
+    else {
+        return 0;
+    }
+};
+
+// To update the product 
+const updateProduct = (id) => {
+    const productToBeUpdate = localStorage_array.find((product) => {
+        if (product.pid == id) {
+            return product;
+        }
+    });
+    const index = localStorage_array.map((product) => product.pid).indexOf(id);
+    sessionStorage.setItem("index", JSON.stringify(index));
+    editValue(productToBeUpdate);
+};
+
+// To render the value in the edit modal section
+const editValue = (productToBeUpdate) => {
+    (pid_update.value = productToBeUpdate.pid),
+        (pname_update.value = productToBeUpdate.pname),
+        (pprice_update.value = productToBeUpdate.pprice),
+        (pdesc_update.value = productToBeUpdate.pdesc);
+    (uploadPreview_update.src = productToBeUpdate.pimage);
+    (pimage_update.value = productToBeUpdate.pimage);
+};
+
+// To change the data which is enetered in the localStorage 
+const edit = () => {
+    const index = JSON.parse(sessionStorage.getItem("index"));
+    const tempData = {
+        pid: pid_update.value,
+        pname: pname_update.value,
+        pprice: pprice_update.value,
+        pimage: uploadPreview_update.src,
+        pdesc: pdesc_update.value,
+    };
+    localStorage_array.splice(index, 1, tempData);
+    setDataToLocalStorage(localStorage_array);
+    const localStorage_getData = getDataFromLocalStorage(localStorage_array);
+    insertProductData(localStorage_getData);
+    swal("", "Product edited!", "success");
 };
